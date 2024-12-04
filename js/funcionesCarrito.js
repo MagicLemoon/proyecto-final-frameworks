b73ea1884414ff44752d5d11b867571bab42dc08
MagicLemoon <167491300+MagicLemoon@users.noreply.github.com>
2024-12-03 22:23:13 -0600

    Add files via upload

---
+++ js/funcionesCarrito.js
@@ -0,0 +1,37 @@
+const appCarrito = Vue.createApp({
+    data() {
+        return {
+            carrito: JSON.parse(localStorage.getItem('carrito')) || []
+        };
+    },
+    methods: {
+        calcularTotalItem(item) {
+            return item.cantidad * item.precioUnitario;
+        },
+        eliminarDelCarrito(index) {
+            if (this.carrito[index].cantidad > 1) {
+                this.carrito[index].cantidad--;
+            } else {
+                this.carrito.splice(index, 1);
+            }
+            this.guardarCarrito();
+        },
+        guardarCarrito() {
+            localStorage.setItem('carrito', JSON.stringify(this.carrito));
+        }
+    },
+    computed: {
+        calcularSubtotal() {
+            return this.carrito.reduce((acc, item) => acc + this.calcularTotalItem(item), 0);
+        },
+        calcularIVA() {
+            return this.calcularSubtotal * 0.16;
+        },
+        calcularTotal() {
+            return this.calcularSubtotal + this.calcularIVA;
+        }
+    },
+    
+});
+
+const carritoApp = appCarrito.mount("#carrito-contenedor");
